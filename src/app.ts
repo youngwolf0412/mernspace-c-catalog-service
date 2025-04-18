313940589c202daa0542749e2fb946674398e5b3
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-18 17:13:51 +0530

    refactor: rename service and update dependencies for catalog management

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import config from "config";
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({ message: config.get("server.port") });
 });
 
 app.use(globalErrorHandler);
